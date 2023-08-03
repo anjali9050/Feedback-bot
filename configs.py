b9415d3153ce04cb8359b61c94ec1d8e0fb94f42
anjali9050 <120652358+anjali9050@users.noreply.github.com>
2023-08-03 22:43:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,15 +2,15 @@ import os
 
 class Config(object):
 
-    API_ID = 4608603 #int(os.environ.get("API_ID", 12345))
+    API_ID = 23560088 #int(os.environ.get("API_ID", 12345))
 
-    API_HASH = "1b339cc7ec86ef9ffd7d0bc8c0a2bda7" #str(os.environ.get("API_HASH", ""))
+    API_HASH = "999c01704d5c417749a98f4b8785fe88" #str(os.environ.get("API_HASH", ""))
 
-    BOT_TOKEN = "5785784298:AAEFpwZfINDSerY0o-cXFyUClsO-8iDLZ48" #str(os.environ.get("BOT_TOKEN", ""))
+    BOT_TOKEN = "6290477228:AAFyWeI_VU8Jx-oCqObhmUTs5en5dnGiawc" #str(os.environ.get("BOT_TOKEN", ""))
     
-    OWNER_ID = 1729070787 #int(os.environ.get("OWNER_ID", 1428968542))
+    OWNER_ID = 5216454450 #int(os.environ.get("OWNER_ID", 1428968542))
 
-    AUTH_USERS = set(int(x) for x in "1428968542 1729070787".split())#set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in "1864861524".split())#set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
 
     START = "Start text"#str(os.environ.get("START_TEXT", ""))
 
@@ -18,15 +18,15 @@ class Config(object):
 
     DONATE = "Donate text"#str(os.environ.get("DONATE_TEXT", ""))
 
-    DONATE_LINK = "https://t.me/HeimanSupports"#str(os.environ.get("DONATE_LINK", ""))
+    DONATE_LINK = "https://t.me/BABA1920Prediction"#str(os.environ.get("DONATE_LINK", ""))
 
-    UPDATE_CHANNEL = "https://t.me/HeimanSupports" #str(os.environ.get("UPDATE_CHANNEL", ""))
+    UPDATE_CHANNEL = "https://t.me/BABA1920Prediction" #str(os.environ.get("UPDATE_CHANNEL", ""))
 
-    SUPPORT_GROUP = "https://t.me/HeimanSupport" #str(os.environ.get("SUPPORT_GROUP", ""))
+    SUPPORT_GROUP = "https://t.me/BABA1920Prediction" #str(os.environ.get("SUPPORT_GROUP", ""))
 
-    DB_URL = "mongodb+srv://heimancreatiin:heimancreatiin@cluster0.ub5udzy.mongodb.net/?retryWrites=true&w=majority" #str(os.environ.get("DB_URL", ""))
+    DB_URL = "mongodb+srv://baba:baba@cluster0.etssakc.mongodb.net/?retryWrites=true&w=majority" #str(os.environ.get("DB_URL", ""))
     
-    DB_NAME = "test" #str(os.environ.get("DB_NAME", ""))
+    DB_NAME = "BroadcastBot" #str(os.environ.get("DB_NAME", ""))
     
     LOG_CHANNEL = -839328944#int(os.environ.get("LOG_CHANNEL", ""))
 
